057a64a3c704069c0284bcfa21e12688593d4cc6
hailinzparra <hailinzparra@gmail.com>
2026-08-14 00:07:08 +0700

    chore: set defaults to false

--- src/ts/data/defaults.ts
+++ src/ts/data/defaults.ts
@@ -8,23 +8,23 @@ export const DEFAULT_SATIN_SESSION: SatinSessionData = {
 }
 
 export const DEFAULT_SATIN_SETTINGS: SatinSettingsData = {
-    global_allow_copy: true,
+    global_allow_copy: false,
 
-    dash_show_openinnewtab_button: true,
+    dash_show_openinnewtab_button: false,
 
     emr_enable_manager: true,
 
-    emr_show_drug_price: true,
+    emr_show_drug_price: false,
     emr_show_drug_price_summary_title: 'Ringkasan Harga',
     emr_show_drug_price_summary_full_title: 'Detail Keuntungan',
     emr_show_drug_price_full_display: false,
     emr_show_drug_price_show_unit_summary: false,
 
-    emr_show_drug_prescriber_name: true,
+    emr_show_drug_prescriber_name: false,
 
-    emr_show_results_menu: true,
+    emr_show_results_menu: false,
 
-    emr_show_cppt_copy_button: true,
+    emr_show_cppt_copy_button: false,
 }
 
 export const DEFAULT_SATIN_POPUP_SETTINGS: SatinPopupSettingsData = {
